831436f92c84899435067715934369e306a68cd3
Drew Repasky <drew@repasky.org>
2022-08-17 08:26:08 -0400

    added new filter `title`, usage: http://localhost:3000/api/items?title=item

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -40,6 +40,7 @@ router.get("/", auth.optional, function(req, res, next) {
   var query = {};
   var limit = 100;
   var offset = 0;
+  var titleFilter = null;
 
   if (typeof req.query.limit !== "undefined") {
     limit = req.query.limit;
@@ -51,6 +52,10 @@ router.get("/", auth.optional, function(req, res, next) {
 
   if (typeof req.query.tag !== "undefined") {
     query.tagList = { $in: [req.query.tag] };
+  }
+
+  if (typeof req.query.title !== "undefined") {
+    titleFilter = req.query.title;
   }
 
   Promise.all([
@@ -70,6 +75,14 @@ router.get("/", auth.optional, function(req, res, next) {
       } else if (req.query.favorited) {
         query._id = { $in: [] };
       }
+
+      if (titleFilter) {
+        // query.title = { $in: ['/' + titleFilter + '/i'] }
+        query.title = { $regex: titleFilter, $options: 'i' }
+        // query.title = { $text: { $search: titleFilter, $caseSensitive: false } };
+      }
+
+      console.log('query', query);
 
       return Promise.all([
         Item.find(query)
